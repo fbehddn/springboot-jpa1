8dc8b727e781127c55383a3655cf9a239e0bfde9
fbehddn <tom0611@naver.com>
2023-08-12 16:52:03 +0900

    회원 조회 API - V2 (DTO)

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -49,7 +49,28 @@ public class MemberApiController {
         return memberService.findMembers();
     }
 
+    @GetMapping("api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
 
+        return new Result(collect);
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @Data
     static class CreateMemberRequest {
